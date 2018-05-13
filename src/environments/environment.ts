9b27671f96cd3b84f65ad6c41f403da4987b3761
samuel koch <samuel.souza.koch@gmail.com>
2018-05-13 20:36:12 -0300

    adicionando environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// The file contents for the current environment will overwrite these during build.
+export const environment = {
+  production: false,
+  api_url: 'https://*****.eadbox.com/api',
+  api_token: '******'
+};
