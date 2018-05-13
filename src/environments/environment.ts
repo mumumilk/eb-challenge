3d2f2b5906fc88cebae771bb6ff43ad0d4fdbcf0
samuel koch <samuel.souza.koch@gmail.com>
2018-05-13 20:34:50 -0300

    removi ambientes

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// The file contents for the current environment will overwrite these during build.
-export const environment = {
-  production: false,
-  api_url: 'https://samuelteste.eadbox.com/api',
-  api_token: '9myz9-rfw21mBg8BsqsM'
-};
